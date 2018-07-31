8530966b71b259138f0500335df32e008292933d
veysel <vinanc72@gmail.com>
2018-08-01 00:07:55 +0300

    Moduls added to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/Home/home.component';
+
+import { HomeService } from './Components/Home/Services/home.service';
+import { HomeDataService } from './Components/Home/Services/home.data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    HomeService,
+    HomeDataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
